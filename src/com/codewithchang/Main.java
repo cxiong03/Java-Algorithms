d5c596e90aca63bf3c3dc2d3de9f64c990e9e5af
Chang <cnxiong03@gmail.com>
2020-12-03 23:45:25 -0500

    creating Hashmap to compare words

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -1,8 +1,24 @@
 package com.codewithchang;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        checkMagazine("give me one grand today night", "give one grand today");
     }
+	    static void checkMagazine(String magazine, String note) {
+            HashMap<String, String> wordMatch = new HashMap<>();
+            String[] noteArray = note.split(" ");
+            for (var word : noteArray) {
+                if (word.equals(" ")) {
+                    continue;
+                }
+
+                if (wordMatch.containsKey(word)) {
+                    wordMatch
+                }
+            }
+
+        }
 }
