db0661ff24088cdf8c84de404197216e4fbd1840
Chang <cnxiong03@gmail.com>
2020-12-04 17:17:44 -0500

    main page

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -1,24 +1,12 @@
 package com.codewithchang;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        checkMagazine("give me one grand today night", "give one grand today");
     }
-	    static void checkMagazine(String magazine, String note) {
-            HashMap<String, String> wordMatch = new HashMap<>();
-            String[] noteArray = note.split(" ");
-            for (var word : noteArray) {
-                if (word.equals(" ")) {
-                    continue;
-                }
-
-                if (wordMatch.containsKey(word)) {
-                    wordMatch
-                }
-            }
-
-        }
 }
