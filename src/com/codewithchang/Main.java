100e6399aa75a131be7946384f26743e9c2eb6e5
Chang <cnxiong03@gmail.com>
2020-12-16 22:09:08 -0500

    reverse a string

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -5,7 +5,14 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        CelsiusToFahrenheit temperature = new CelsiusToFahrenheit();
-        temperature.Fahrenheit();
+        String secretWord = "Tenet";
+
+        byte[] strAsByteArray = secretWord.getBytes();
+        byte[] result = new byte[strAsByteArray.length];
+
+        for (int i = 0; i < strAsByteArray.length; i++) {
+            result[i] = strAsByteArray[strAsByteArray.length - i -1];
+            System.out.println(new String(result));
+        }
     }
 }
