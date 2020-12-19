47c51e029359ce5ca789d532643183add2894a1e
Chang <cnxiong03@gmail.com>
2020-12-18 19:49:36 -0500

    iterative solution

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,16 +3,36 @@ package com.codewithchang;
 import java.util.*;
 
 public class Main {
-// Driver method
     public static void main(String[] args) {
-        int num = 5;
-        System.out.println("Factorial of "+ num + " is " + factorial(5));
-    }
-// method to find factorial of given number
-    static int factorial(int n) {
-        if (n == 0)
-            return 1;
+        // Iterative solution
+        int num = 6;
+        System.out.println("Factorial of " + num + " is " + factorial(6));
+        }
 
-        return n * factorial(n-1);
+        static int factorial(int n) {
+            int res = 1, i;
+            for (i = 2; i <= n; i++) {
+                res *= i;
+            }
+            return res;
+
+
+//        -------------------------
+
+// Driver method
+//        int num = 5;
+//        System.out.println("Factorial of "+ num + " is " + factorial(5));
     }
+
+
+
+//    ----------------------------
+
+// method to find factorial of given number
+//    static int factorial(int n) {
+//        if (n == 0)
+//            return 1;
+//
+//        return n * factorial(n-1);
+//    }
 }
