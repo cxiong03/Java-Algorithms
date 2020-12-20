b5f27e2adc6e13902ddd1e2a8f66eecde776fb98
Chang <cnxiong03@gmail.com>
2020-12-19 19:14:39 -0500

    ternary solution

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -4,29 +4,40 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+//        One line solution (Using ternary operator)
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+
+
         // Iterative solution
-        int num = 6;
-        System.out.println("Factorial of " + num + " is " + factorial(6));
+//        int num = 6;
+//        System.out.println("Factorial of " + num + " is " + factorial(6));
         }
-
-        static int factorial(int n) {
-            int res = 1, i;
-            for (i = 2; i <= n; i++) {
-                res *= i;
-            }
-            return res;
-
-
-//        -------------------------
-
-// Driver method
-//        int num = 5;
-//        System.out.println("Factorial of "+ num + " is " + factorial(5));
+// Ternary solution
+    static int factorial(int n) {
+        return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
     }
 
 
 
-//    ----------------------------
+//        Iterative solution
+//        static int factorial(int n) {
+//            int res = 1, i;
+//            for (i = 2; i <= n; i++) {
+//                res *= i;
+//            }
+//            return res;
+
+
+//        -------------------------
+
+    // Recursive solution
+// Driver method
+//        int num = 5;
+//        System.out.println("Factorial of "+ num + " is " + factorial(5));
+
+
+    // Recursive solution
 
 // method to find factorial of given number
 //    static int factorial(int n) {
